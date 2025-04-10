655055493b2b8068fcebe43fa634c91539fbb1ed
ManMac32i <manuel.macis@icloud.com>
2025-04-10 11:27:42 +0200

    Arrays

--- Esercitazione_04_ARR/main.cpp
+++ Esercitazione_04_ARR/main.cpp
@@ -1,5 +1,34 @@
+# include <iostream>
+# include <fstream>
+# include <string>
+# include <iomanip>
+# include <vector>
+
+using namespace std;
+using namespace Eigen;
+
+double value(double& rate, double& investment)
+{
+    return (1 + rate) * investment;
+}
+
 int main()
 {
+    
+    std::ifstream file("data.txt");
+    std::ofstream results("results.txt");
+
+    if (!file || !results) {
+        cout << "Error opening data.txt or results.txt" << endl; // if the file is not correctly opened 
+    }
+
+    double investment;
+    double assets;
+    VectorXd rate;
+
+    file >> investment;
+    file >> assets;
+    while(file )
     return 0;
 }
 
