db33664451ec90c4d9135f878181ecb2ab9bfff8
ManMac32i <manuel.macis@icloud.com>
2025-04-11 10:30:22 +0200

    Read and analize csv files

--- Esercitazione_05_STL/main.cpp
+++ Esercitazione_05_STL/main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main()
 {
-    ifstream Cell0Ds("Cell0Ds.csv");
-    ifstream Cell1Ds("Cell1Ds.csv");
-    ifstream Cell2Ds("Cell2Ds.csv");
-    
+    ifstream Cell0Ds("PolygonalMesh/Cell0Ds.csv");
+    ifstream Cell1Ds("PolygonalMesh/Cell1Ds.csv");
+    ifstream Cell2Ds("PolygonalMesh/Cell2Ds.csv");
+
     return 0;
 }
--- Esercitazione_05_STL/src/Utils.cpp
+++ Esercitazione_05_STL/src/Utils.cpp
@@ -4,17 +4,31 @@
 
 using namespace std;
 
+string cut_commas(unsigned int& pointer, string& s)
+{
+    unsigned int location = s.find(',');
+    s = s.substr(pointer, location);
+    pointer = location;
+    return s = s.substr(pointer + 1, s.length());
+}
+
 int analyze_data(ifstream& file)
 {
     string line;
     vector<int> Id, Marker;
     vector<double> X,Y;
+    unsigned int location = 0;
 
     getline(file, line);
 
     while(getline(file, line))
     {
-        
+        Id.push_back(stoi(cut_commas(location, line)));
+        Marker.push_back(stoi(cut_commas(location, line)));
+        X.push_back(stod(cut_commas(location, line)));
+        Y.push_back(stod(line));
+
+        location = 0;
     }
 
     return 0;
