683583078de046a475876812491451a537a6eaf8
ManMac32i <manuel.macis@icloud.com>
2025-04-12 17:04:16 +0200

    Csv read files

--- Esercitazione_05_STL/src/Utils.cpp
+++ Esercitazione_05_STL/src/Utils.cpp
@@ -9,10 +9,10 @@ string cut_commas(unsigned int& pointer, string& s)
     unsigned int location = s.find(',');
     s = s.substr(pointer, location);
     pointer = location;
-    return s = s.substr(pointer + 1, s.length());
+    return s.substr(pointer + 1, s.length());
 }
 
-int analyze_data(ifstream& file)
+int analyze_Cell0_data(ifstream& file)
 {
     string line;
     vector<int> Id, Marker;
@@ -27,6 +27,29 @@ int analyze_data(ifstream& file)
         Marker.push_back(stoi(cut_commas(location, line)));
         X.push_back(stod(cut_commas(location, line)));
         Y.push_back(stod(line));
+
+        location = 0;
+    }
+
+    return 0;
+}
+
+int analyze_Cell2_data(ifstream& file)
+{
+    string line;
+    vector<int> Id, Marker, Numvertices, Vertices, NumEdges, Edges;
+    unsigned int location = 0;
+
+    getline(file, line);
+
+    while(getline(file, line))
+    {
+        Id.push_back(stoi(cut_commas(location, line)));
+        Marker.push_back(stoi(cut_commas(location, line)));
+        Marker.push_back(stoi(cut_commas(location, line)));
+        Marker.push_back(stoi(cut_commas(location, line)));
+        Marker.push_back(stoi(cut_commas(location, line)));
+        Marker.push_back(stoi(line));
 
         location = 0;
     }
