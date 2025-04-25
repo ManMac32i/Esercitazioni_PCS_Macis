1856f31c4d577227562dc90ae2af2e1231a30fb5
ManMac32i <manuel.macis@icloud.com>
2025-04-25 13:15:15 +0200

    SelectionSort

--- Esercitazione_06_SORT/src/SortingAlgorithm.hpp
+++ Esercitazione_06_SORT/src/SortingAlgorithm.hpp
@@ -6,6 +6,31 @@
 using namespace std;
 
 namespace SortLibrary {
+    template <typename T>
+    concept sortable = requires(T& t) {
+        {t < t} -> std::convertible_to<bool>;
+    };
+
+    template<sortable T>
+    void SelectionSort(vector<T> & v)
+    {
+        const unsigned int n = v.size();
+        for (int i = 0, i < n - 1, i++)
+        {
+            unsigned int min_index = i;
+            for (int j = i + 1, i < n, j++)
+            {
+                if (v[j] < v[min_index])
+                {
+                    min_index = j;
+                }
+
+            }
+            T swap = v[i];
+            v[i] = v[min_index];
+            v[min_index] = swap;
+        }
+    }
 
 
 }
